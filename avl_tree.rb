9edc99aa1e3bd4c0386de60da791df7b36b015ef
Dan Murphy <hello@danmurphy.codes>
2020-05-08 12:00:50 -0600

    Super messy implementation of AVL tree rotations

--- avl_tree.rb
+++ avl_tree.rb
@@ -15,18 +15,34 @@ class AVLTree
     new_node = Node.new(value)
     return self.root = new_node if root.nil?
 
-    previous_node = nil
+    parent = nil
 
     binary_search(root, value) do |node|
-      previous_node = node unless node.nil?
+      parent = node unless node.nil?
     end
 
-    new_node.parent = previous_node
+    new_node.parent = parent
+    child           = new_node
 
-    if value <= previous_node.value
-      previous_node.left = new_node
+    if value <= parent.value
+      parent.left = new_node
     else
-      previous_node.right = new_node
+      parent.right = new_node
+    end
+
+    until parent.nil? do
+      balance_adjustment = child == parent.left ? -1 : 1
+      parent.balance += balance_adjustment
+
+      if parent.violating_balance_invariant?
+        rotate(parent)
+        break
+      end
+
+      break if parent.balanced?
+
+      child = parent
+      parent = parent.parent
     end
 
     new_node
@@ -36,6 +52,7 @@ class AVLTree
     return if node.nil?
 
     parent = node.parent
+    is_left_child = parent.left == node unless parent.nil?
 
     if node.children.none?
       if node.root?
@@ -55,17 +72,44 @@ class AVLTree
       else
         parent.left = child
       end
+
+      child.parent = parent
     else
       if node.root? || node == parent.right
         node.value = node.in_order_successor.value
-        delete(node.in_order_successor)
+        return delete(node.in_order_successor)
       else
         node.value = node.in_order_predecessor.value
-        delete(node.in_order_predecessor)
+        return delete(node.in_order_predecessor)
       end
     end
 
+    # TODO: DRY this out with the logic in `insert`. Just need an argument to
+    # flip the sign of the balance adjustment.
+    until parent.nil? do
+      (is_left_child = parent.left == node) if is_left_child.nil?
+
+      balance_adjustment = is_left_child ? 1 : -1
+      parent.balance += balance_adjustment
+
+      if parent.violating_balance_invariant?
+        rotate(parent)
+        break
+      end
+
+      is_left_child = nil
+      node = parent
+      parent = parent.parent
+    end
+
     true
+  end
+
+  def print
+    # TODO: figure out the depth or approximate depth of the tree
+    # Maybe call #each, add all nodes to an array
+    # indent root node based on how deep tree is
+    # indent subsequent layers relative to the root
   end
 
   private
@@ -79,7 +123,7 @@ class AVLTree
     attr_accessor :value, :parent, :left, :right, :balance
 
     def children
-      [left, right]
+      [left, right].compact
     end
 
     # TODO: take notes. Why is it the case that right.left always produces the
@@ -98,6 +142,26 @@ class AVLTree
 
     def root?
       parent.nil?
+    end
+
+    def balanced?
+      balance.zero?
+    end
+
+    def imbalanced?
+      !balanced?
+    end
+
+    def violating_balance_invariant?
+      (balance < -1) || (balance > 1)
+    end
+
+    def right_heavy?
+      balance > 1
+    end
+
+    def left_heavy?
+      balance < 1
     end
   end
 
@@ -123,5 +187,180 @@ class AVLTree
     end
 
     binary_search(node, value, &block)
+  end
+
+  def rotate(node)
+    if node.right_heavy?
+      child = node.right
+      opposite_direction_grandchild = child.left
+      same_direction_grandchild = child.right
+
+      if same_direction_grandchild && opposite_direction_grandchild.nil?
+        # Pre-rotation
+        # (1)
+        #    \
+        #     (2)
+        #        \
+        #         (3)
+
+        # Post-rotation
+        #      (2)
+        #    /    \
+        # (1)      (3)
+        swap_parents_of_node_and_child(node, child)
+        child.left = node
+        node.parent = child
+        node.right = nil
+
+        node.balance = 0
+        child.balance = 0
+      elsif same_direction_grandchild.nil? && opposite_direction_grandchild
+        # Pre-rotation
+        # (1)
+        #    \
+        #     (3)
+        #    /
+        # (2)
+
+        # Intermediate step to make it so a second rotation can balance the tree
+        # (1)
+        #   \
+        #    (2)
+        #      \
+        #      (3)
+        # TODO: can we eliminate some of these steps?
+        swap_parents_of_node_and_child(child, opposite_direction_grandchild)
+        opposite_direction_grandchild.right = child
+        child.parent = opposite_direction_grandchild
+        child.left = nil
+        node.right = opposite_direction_grandchild
+
+        # Post-rotation
+        #      (2)
+        #    /    \
+        # (1)      (3)
+        swap_parents_of_node_and_child(node, opposite_direction_grandchild)
+        opposite_direction_grandchild.left = node
+        node.parent = opposite_direction_grandchild
+        node.right = nil
+
+        node.balance = 0
+        child.balance = 0
+      elsif same_direction_grandchild && opposite_direction_grandchild
+        # Pre-rotation
+        # (1)
+        #    \
+        #     (3)
+        #    /   \
+        #  (2)    (5)
+
+        # Post-rotation
+        #      (3)
+        #    /    \
+        # (1)      (5)
+        #    \
+        #     (2)
+        swap_parents_of_node_and_child(node, child)
+        child.left = node
+        node.parent = child
+        node.right = opposite_direction_grandchild
+        opposite_direction_grandchild.parent = node
+
+        node.balance = 1
+        child.balance = -1
+      end
+    elsif node.left_heavy?
+      child = node.left
+      opposite_direction_grandchild = child.right
+      same_direction_grandchild = child.left
+
+      if same_direction_grandchild && opposite_direction_grandchild.nil?
+        # Pre-rotation
+        #         (3)
+        #        /
+        #     (2)
+        #    /
+        # (1)
+
+        # Post-rotation
+        #      (2)
+        #    /    \
+        # (1)      (3)
+        swap_parents_of_node_and_child(node, child)
+        child.right = node
+        node.parent = child
+        node.left = nil
+
+        node.balance = 0
+        child.balance = 0
+      elsif same_direction_grandchild.nil? && opposite_direction_grandchild
+        # Pre-rotation
+        #         (3)
+        #        /
+        #     (1)
+        #        \
+        #         (2)
+
+        # Intermediate step to make it so a second rotation can balance the tree
+        #         (3)
+        #        /
+        #     (2)
+        #    /
+        # (1)
+        swap_parents_of_node_and_child(child, opposite_direction_grandchild)
+        opposite_direction_grandchild.left = child
+        child.right = nil
+        child.parent = opposite_direction_grandchild
+        node.left = opposite_direction_grandchild
+
+        # Post-rotation
+        #      (2)
+        #    /    \
+        # (1)      (3)
+        swap_parents_of_node_and_child(node, opposite_direction_grandchild)
+        opposite_direction_grandchild.right = node
+        node.parent = opposite_direction_grandchild
+        node.left = nil
+
+        node.balance = 0
+        child.balance = 0
+      elsif same_direction_grandchild && opposite_direction_grandchild
+        # Pre-rotation
+        #            (5)
+        #          /
+        #       (3)
+        #     /   \
+        #  (2)    (4)
+
+        # Post-rotation
+        #      (3)
+        #    /    \
+        # (2)      (5)
+        #         /
+        #     (4)
+        swap_parents_of_node_and_child(node, child)
+        child.right = node
+        node.parent = child
+        node.left = opposite_direction_grandchild
+        opposite_direction_grandchild.parent = node
+
+        node.balance = -1
+        child.balance = 1
+      end
+    end
+  end
+
+  def swap_parents_of_node_and_child(node, child)
+    parent = node.parent
+
+    if parent.nil?
+      self.root = child
+    elsif node == parent.right
+      parent.right = child
+    elsif node == parent.left
+      parent.left = child
+    end
+
+    child.parent = parent
   end
 end
--- spec/avl_tree_spec.rb
+++ spec/avl_tree_spec.rb
@@ -1,71 +1,83 @@
 require "rspec"
 require "./avl_tree"
-require "pry"
 
 describe AVLTree do
-  subject(:integer_tree) do
-    tree = described_class.new 
-    tree.insert(5)
-    tree.insert(7)
-    tree.insert(1)
-    tree.insert(-3)
-    tree.insert(33)
-    tree.insert(7)
-    tree.insert(9)
+  subject(:tree) do
+    t = described_class.new
 
+    t.insert(5)
+    expect(t.root.balance).to be 0
+
+    t.insert(7)
+    expect(t.root.balance).to be 1
+
+    t.insert(1)
+    expect(t.root.balance).to be 0
+
+    t.insert(-3)
+    expect(t.root.balance).to be -1
+
+    t.insert(33)
+    expect(t.root.balance).to be 0
+
+    t.insert(7)
+    expect(t.root.balance).to be 0
+
+    t.insert(9)
+    expect(t.root.balance).to be 1
     #            5
     #          /   \
-    #         1     7 
+    #         1     7
     #        /     / \
     #      -3     7   33
-    #                / 
-    #               9 
-    #                   
-    tree
+    #                /
+    #               9
+    t
   end
 
   it "inserts nodes in sorted order by updating references to/from other nodes" do
-    expect(integer_tree.root.value).to be 5
+    expect(tree.root.value).to be 5
 
     # Left subtree
-    expect(integer_tree.root.left.value).to be 1
-      # Left -> Left
-      expect(integer_tree.root.left.left.value).to be -3
-        # Left -> Left -> Left
-        expect(integer_tree.root.left.left.left).to be nil
-        # Left -> Left -> Right
-        expect(integer_tree.root.left.left.right).to be nil
+    expect(tree.root.left.value).to be 1
 
-      # Left -> Right
-      expect(integer_tree.root.left.right).to be nil
+    # Left -> Left
+    expect(tree.root.left.left.value).to be -3
+    # Left -> Left -> Left
+    expect(tree.root.left.left.left).to be nil
+    # Left -> Left -> Right
+    expect(tree.root.left.left.right).to be nil
+
+    # Left -> Right
+    expect(tree.root.left.right).to be nil
 
     # Right subtree
-    expect(integer_tree.root.right.value).to be 7
+    expect(tree.root.right.value).to be 7
 
-      # Right -> Left
-      expect(integer_tree.root.right.left.value).to be 7
-        # Right -> Left -> Left
-        expect(integer_tree.root.right.left.left).to be nil
-        # Right -> Left -> Right
-        expect(integer_tree.root.right.left.right).to be nil
+    # Right -> Left
+    expect(tree.root.right.left.value).to be 7
+    # Right -> Left -> Left
+    expect(tree.root.right.left.left).to be nil
+    # Right -> Left -> Right
+    expect(tree.root.right.left.right).to be nil
 
-      # Right -> Right
-      expect(integer_tree.root.right.right.value).to be 33
-        # Right -> Right -> Left
-        expect(integer_tree.root.right.right.left.value).to be 9
-          # Right -> Right -> Left -> Right
-          expect(integer_tree.root.right.right.left.right).to be nil
-          # Right -> Right -> Left -> Left
-          expect(integer_tree.root.right.right.left.left).to be nil
+    # Right -> Right
+    expect(tree.root.right.right.value).to be 33
+    # Right -> Right -> Left
+    expect(tree.root.right.right.left.value).to be 9
+    # Right -> Right -> Left -> Right
+    expect(tree.root.right.right.left.right).to be nil
+    # Right -> Right -> Left -> Left
+    expect(tree.root.right.right.left.left).to be nil
 
-        # Right -> Right -> Right
-        expect(integer_tree.root.right.right.right).to be nil
+    # Right -> Right -> Right
+    expect(tree.root.right.right.right).to be nil
   end
 
   it "iterates over nodes in order" do
     values = []
 
-    integer_tree.each do |node|
+    tree.each do |node|
       values << node.value
     end
 
@@ -73,126 +85,229 @@ describe AVLTree do
   end
 
   it "finds nodes by value" do
-    node = integer_tree.find(7)
+    node = tree.find(7)
     expect(node.value).to be 7
     expect(node.left.value).to be 7
     expect(node.right.value).to be 33
   end
 
   it "deletes a node with no children" do
-    # Left side
-    node = integer_tree.find(-3)
-    expect(integer_tree.delete(node)).to be true
+    expect(tree).not_to receive(:rotate)
+    # Right side
+    node = tree.find(9)
+    expect(tree.delete(node)).to be true
 
     values = []
 
-    integer_tree.each do |node|
+    tree.each do |node|
       values << node.value
     end
 
-    expect(values).to eq [1, 5, 7, 7, 9, 33]
+    expect(values).to eq [-3, 1, 5, 7, 7, 33]
+    expect(tree.root.right.right.value).to be 33
 
-    # Right side
-    node = integer_tree.find(9)
-    expect(integer_tree.delete(node)).to be true
+    # Left side
+    node = tree.find(-3)
+    tree.delete(node)
 
     values = []
 
-    integer_tree.each do |node|
+    tree.each do |node|
       values << node.value
     end
 
     expect(values).to eq [1, 5, 7, 7, 33]
-    expect(integer_tree.root.right.right.value).to be 33
   end
 
-
   it "deletes a node with one child" do
-    # Left side
-    node = integer_tree.find(1)
-    expect(integer_tree.delete(node)).to be true
+    expect(tree).not_to receive(:rotate)
+
+    # Right side
+    node = tree.find(33)
+    expect(tree.delete(node)).to be true
 
     values = []
 
-    integer_tree.each do |node|
+    tree.each do |node|
       values << node.value
     end
 
-    expect(values).to eq [-3, 5, 7, 7, 9, 33]
+    expect(values).to eq [-3, 1, 5, 7, 7, 9]
 
-    # Right side
-    node = integer_tree.find(33)
-    expect(integer_tree.delete(node)).to be true
+    # Left side
+    node = tree.find(1)
+    expect(tree.delete(node)).to be true
 
     values = []
 
-    integer_tree.each do |node|
+    tree.each do |node|
       values << node.value
     end
 
     expect(values).to eq [-3, 5, 7, 7, 9]
-    expect(integer_tree.root.right.right.value).to be 9
   end
 
   it "deletes a node with two children" do
-    node = integer_tree.find(7)
-    expect(integer_tree.delete(node)).to be true
+    expect(tree).not_to receive(:rotate)
+
+    node = tree.find(7)
+    expect(tree.delete(node)).to be true
 
     values = []
 
-    integer_tree.each do |node|
+    tree.each do |node|
       values << node.value
     end
 
     expect(values).to eq [-3, 1, 5, 7, 9, 33]
 
-    expect(integer_tree.root.value).to be 5
+    expect(tree.root.value).to be 5
 
     # Left subtree
-    expect(integer_tree.root.left.value).to be 1
-      # Left -> Left
-      expect(integer_tree.root.left.left.value).to be -3
-        # Left -> Left -> Left
-        expect(integer_tree.root.left.left.left).to be nil
-        # Left -> Left -> Right
-        expect(integer_tree.root.left.left.right).to be nil
+    expect(tree.root.left.value).to be 1
+    # Left -> Left
+    expect(tree.root.left.left.value).to be -3
+    # Left -> Left -> Left
+    expect(tree.root.left.left.left).to be nil
+    # Left -> Left -> Right
+    expect(tree.root.left.left.right).to be nil
 
-      # Left -> Right
-      expect(integer_tree.root.left.right).to be nil
+    # Left -> Right
+    expect(tree.root.left.right).to be nil
 
     # Right subtree
-    expect(integer_tree.root.right.value).to be 9
+    expect(tree.root.right.value).to be 9
 
-      # Right -> Left
-      expect(integer_tree.root.right.left.value).to be 7
-        # Right -> Left -> Left
-        expect(integer_tree.root.right.left.left).to be nil
-        # Right -> Left -> Right
-        expect(integer_tree.root.right.left.right).to be nil
+    # Right -> Left
+    expect(tree.root.right.left.value).to be 7
+    # Right -> Left -> Left
+    expect(tree.root.right.left.left).to be nil
+    # Right -> Left -> Right
+    expect(tree.root.right.left.right).to be nil
 
-      # Right -> Right
-      expect(integer_tree.root.right.right.value).to be 33
-        # Right -> Right -> Left
-        expect(integer_tree.root.right.right.left).to be nil
-        # Right -> Right -> Right
-        expect(integer_tree.root.right.right.right).to be nil
+    # Right -> Right
+    expect(tree.root.right.right.value).to be 33
+    # Right -> Right -> Left
+    expect(tree.root.right.right.left).to be nil
+    # Right -> Right -> Right
+    expect(tree.root.right.right.right).to be nil
   end
 
   it "deletes the root" do
-    expect(integer_tree.delete(integer_tree.root)).to be true
+    expect(tree.delete(tree.root)).to be true
 
     values = []
 
-    integer_tree.each do |node|
+    tree.each do |node|
       values << node.value
     end
 
     expect(values).to eq [-3, 1, 7, 7, 9, 33]
-    root = integer_tree.root
+    root = tree.root
     expect(root.value).to be 7
     expect(root.left.value).to be 1
-    expect(root.right.value).to be 7
+    expect(root.right.value).to be 9
   end
 
-  it "rotates subtrees to maintain balance"
+  # Confused? Watch this!
+  # https://www.cs.usfca.edu/~galles/visualization/AVLtree.html
+  context "rotating nodes to keep subtree heights within 1 of each other" do
+    it "rotates right-heavy subtrees to maintain balance" do
+      node = tree.insert(8)
+      expect(node.parent.value).to be 9
+      expect(node.parent.right.value).to be 33
+      # Without rebalancing we'd end up with a parent of 9 for the new node:
+      #            5
+      #          /   \
+      #         1     7
+      #        /     / \
+      #      -3     7   33
+      #                /
+      #               9
+      #             /
+      #            8
+      # Which violates our specified invariant that the height of the subtrees
+      # for a given node (33 in this case, because it is the nearest ancestor
+      # of the new node for whom the invariant is violated) cannot differ by
+      # more than 1.
+      # So we rebalance the right subtree such that it looks like:
+      #            5
+      #          /   \
+      #         1     7
+      #        /     / \
+      #      -3     7   9
+      #                / \
+      #               8   33
+
+      node = tree.insert(16)
+      expect(node.parent.value).to be 33
+      expect(node.parent.parent.value).to be 9
+      expect(node.parent.parent.left.value).to be 7
+      expect(node.parent.parent.left.left.value).to be 7
+      expect(node.parent.parent.left.right.value).to be 8
+      # This time without rebalancing we'd end up with the right subtree of 7
+      # differing from the left by more than 1. So we must rotate again:
+      #             5
+      #          /    \
+      #         1       9
+      #        /     /    \
+      #      -3     7      33
+      #           /  \    /
+      #          7    8  16
+      # The rotation algorithm goes like this:
+      # traverse up parent-by-parent from the newly inserted node until you find
+      # one for whom the invariant is violated. Once you do, you know that the
+      # violation will be a difference in subtree height of exactly 1, because
+      # any existing violation would've been corrected by a previous rotation.
+      # If the node is right-heavy we need to "replace" it by its first child
+      # on the right side as in the diagram above and vice versa for the left side
+      values = []
+
+      tree.each do |node|
+        values << node.value
+      end
+
+      expect(values).to eq [-3, 1, 5, 7, 7, 8, 9, 16, 33]
+    end
+
+    it "rotates left-heavy subtrees to maintain balance"
+    it "makes child nodes grandchildren when a rotation leads to a new child being added to a node"
+    # Eg if 11 was just inserted;
+    #             5
+    #          /    \
+    #         0       7
+    #              /    \
+    #             6      9
+    #                      \
+    #                       11
+    # 11's grandparent 7 is actually not in violation of the invariant constraint.
+    # But, as we traverse upwards, we find that the great-grandparent 5 is in violation.
+    # So we need to rotate 7 up and make 5 the left child of 7. But 7 already
+    # has a left child, 6. So, we make 6 the right child of 5.
+    #                 7
+    #              /    \
+    #             5      9
+    #            / \       \
+    #           0   6       11
+    it "rotates after deleting a node leaves the tree unbalanced"
+    # Eg if we delete 0;
+    #             5
+    #          /    \
+    #         0       7
+    #              /    \
+    #             6      9
+    # We traverse upwards to 5 and see that it is in violation of the invariant constraint:
+    #             5
+    #               \
+    #                 7
+    #              /    \
+    #             6      9
+    # So we need to rotate 7 up and make 5 the left child of 7. But 7 already
+    # has a left child, 6. So, we make 6 the right child of 5.
+    #                 7
+    #              /    \
+    #             5      9
+    #              \       \
+    #               6       11
+  end
 end
